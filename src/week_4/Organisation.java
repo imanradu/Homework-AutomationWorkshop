acced843af822fa2c55bede0be5d8e99971bba48
Radu Iman <radu.iman94@gmail.com>
2019-10-11 09:37:44 +0300

    Fix the issues from mainMenu and continue the implemenation

--- src/week_4/Organisation.java
+++ src/week_4/Organisation.java
@@ -23,44 +23,69 @@ public class Organisation extends Employee {
 
     public void searchEmployee(HashMap employees) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Search: ");
-//        employees.containsValue(input.next());
+        System.out.print("Search for employee: ");
+        for (int id = 0; id < employees.size(); id ++) {
+//            String employee = (String) employees.values().toArray()[id].name;
+//            if (employee == input.next()) {
+//                System.out.println(employee);
+            }
+
+        }
     }
 
     public void removeEmployee(HashMap employees) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Remove: ");
-//        employees.remove(input.next());
+        System.out.println("Remove employee: ");
+        input.next();
     }
 
     public void updateEmployee(HashMap employees) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Update employee: ");
+//        employees.input.next();
+        System.out.println("Field: ");
+//        input.next();
+        System.out.println("Value: ");
+//        input.next();
     }
 
-    public int mainMenu() {
+    public void mainMenu(HashMap employees) {
         System.out.println(">>> Organization <<<");
         System.out.println("1. Adding employees");
         System.out.println("2. Searching for employees");
         System.out.println("3. Removing employees");
         System.out.println("4. Updating employee information");
+        System.out.println("5. Exit from the MainMenu");
+        System.out.println("Choose an option: ");
         Scanner input = new Scanner(System.in);
-        return input.nextInt();
-    }
-
-    public static void main(String[] args) {
-        HashMap<Integer,Employee> employees=new HashMap<>();
         Organisation Softvision = new Organisation();
 
-        switch (Softvision.mainMenu()) {
+        int menuOption = input.nextInt();
+        switch (menuOption) {
             case 1:
                 Softvision.addEmployee(employees);
+                mainMenu(employees);
+                break;
             case 2:
                 Softvision.searchEmployee(employees);
+                mainMenu(employees);
+                break;
             case 3:
                 Softvision.removeEmployee(employees);
+                mainMenu(employees);
+                break;
             case 4:
                 Softvision.updateEmployee(employees);
+                mainMenu(employees);
+                break;
+            case 5:
+                break;
         }
     }
 
+    public static void main(String[] args) {
+        Organisation org = new Organisation();
+        HashMap<Integer, Employee> employees = new HashMap<>();
+        org.mainMenu(employees);
+    }
 }
