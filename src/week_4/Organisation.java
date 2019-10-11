6171a700bff44e63764e317ff1d46279f4a7e9b6
Radu Iman <radu.iman94@gmail.com>
2019-10-11 08:33:28 +0300

    Implement the MainMenu to choose for adding, searching, removing or updating employees

--- src/week_4/Organisation.java
+++ src/week_4/Organisation.java
@@ -1,10 +1,66 @@
 package week_4;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Organisation extends Employee {
+    public void addEmployee(HashMap employees) {
+        Employee newEmployee = new Employee();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Name: ");
+        newEmployee.setName(input.next());
+        System.out.print("Gender: ");
+        newEmployee.setGender(input.next().charAt(0));
+        System.out.print("Age: ");
+        newEmployee.setAge(input.nextInt());
+        System.out.print("Department: ");
+        newEmployee.setDepartment(input.next());
+        System.out.print("Job Title: ");
+        newEmployee.setJobTitle(input.next());
+        employees.put(employees.size() + 1, newEmployee);
+    }
+
+    public void searchEmployee(HashMap employees) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Search: ");
+//        employees.containsValue(input.next());
+    }
+
+    public void removeEmployee(HashMap employees) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Remove: ");
+//        employees.remove(input.next());
+    }
+
+    public void updateEmployee(HashMap employees) {
+
+    }
+
+    public int mainMenu() {
+        System.out.println(">>> Organization <<<");
+        System.out.println("1. Adding employees");
+        System.out.println("2. Searching for employees");
+        System.out.println("3. Removing employees");
+        System.out.println("4. Updating employee information");
+        Scanner input = new Scanner(System.in);
+        return input.nextInt();
+    }
+
     public static void main(String[] args) {
-        Employee x = new Employee();
-        x.setName("Radu");
-        System.out.println(x.getName());
+        HashMap<Integer,Employee> employees=new HashMap<>();
+        Organisation Softvision = new Organisation();
+
+        switch (Softvision.mainMenu()) {
+            case 1:
+                Softvision.addEmployee(employees);
+            case 2:
+                Softvision.searchEmployee(employees);
+            case 3:
+                Softvision.removeEmployee(employees);
+            case 4:
+                Softvision.updateEmployee(employees);
+        }
     }
 
 }
